2c8f7f1472b1873059cf361603faac36d702cf23
jedvincent <jedvince@gmail.com>
2024-07-10 10:43:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
-
 import pandas as pd
 
 
@@ -13,7 +12,6 @@ st.write("""Choose the fruits you want in your smoothie!
          """
 )
 
-import streamlit as st
 
 name_on_order = st.text_input("Name on Smoothie: ")
 st.write("The name on your smoothie will be: ", name_on_order)
@@ -31,6 +29,7 @@ pd_df = my_dataframe.to_pandas()
 ingredients_list = st.multiselect(
     'choose up to 5 ingredients:'
     , my_dataframe
+    ,max_selections=5
 )
 
 if ingredients_list:
